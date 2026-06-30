d93c0ee13c2e5b212f5393edf736b79f75e0ff26
Piyush-Pawar-079 <piyush.pawar@zeuslearning.com>
2026-06-30 07:25:42 +0000

    Reddis error hadled in try catch

--- traineeManagementAPI/Service/RedisService/RedisService.cs
+++ traineeManagementAPI/Service/RedisService/RedisService.cs
@@ -3,33 +3,56 @@ using Microsoft.Extensions.Caching.Distributed;
 
 namespace traineeManagementAPI.Service.RedisService;
 
-public class RedisService(IDistributedCache db): IRedisService
+public class RedisService(IDistributedCache db, ILogger<RedisService> logger): IRedisService
 {
     private readonly IDistributedCache _db = db;
+    private readonly ILogger<RedisService> _logger = logger;
 
     public async Task<T?> GetAsync<T>(string key)
     {
-        var value = await _db.GetStringAsync(key);
-        if (string.IsNullOrEmpty(value))
+        try
         {
+            var value = await _db.GetStringAsync(key);
+            if (string.IsNullOrEmpty(value))
+            {
+                return default;
+            }
+            return JsonSerializer.Deserialize<T>(value);
+        }
+        catch (Exception ex)
+        {
+            _logger.LogError("Something went wrong while retrieveing data from the cache for the key - " + key + ex);
             return default;
         }
-        return JsonSerializer.Deserialize<T>(value);
     }
 
     public async Task RemoveAsync(string key)
     {
-        await _db.RemoveAsync(key);
+        try
+        {
+            await _db.RemoveAsync(key);
+        }
+        catch (Exception ex)
+        {
+            _logger.LogError("Something went wrong while removing data from the cache for the key - " + key + ex);
+        }
     }
 
     public async Task SetAsync<T>(string key, T value, double? expiry = null)
     {
-        var json = JsonSerializer.Serialize(value);
-        var options = new DistributedCacheEntryOptions
+        try
         {
-            AbsoluteExpirationRelativeToNow = TimeSpan.FromMinutes(expiry ?? 10)
-        };
-        await _db.SetStringAsync(key, json, options);
+            var json = JsonSerializer.Serialize(value);
+            var options = new DistributedCacheEntryOptions
+            {
+                AbsoluteExpirationRelativeToNow = TimeSpan.FromMinutes(expiry ?? 10)
+            };
+            await _db.SetStringAsync(key, json, options);
+        }
+        catch (Exception ex)
+        {
+            _logger.LogError("Something went wrong while saving data from the cache for the key - " + key + ex);
+        }
     }
 
 }
